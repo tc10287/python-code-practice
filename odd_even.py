e5f6a1532cf186c285e270237743f9d8adf2eac7
tc10287 <120434943+tc10287@users.noreply.github.com>
2023-08-09 19:31:49 -0700

    Add files via upload
    i know this is a simple exercise but i'm trying to refresh my notes and practicing, and documenting this in my github to retain in my memory and won't lose it

---
+++ odd_even.py
@@ -0,0 +1,7 @@
+def even_check(num):
+    if num%2==0:   #using the logic of modulus where the remainder for even number is always 0
+        return(print("even number"))
+    else:
+        return(print("odd number"))
+even_check(23)
+    
