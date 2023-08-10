74a50ce9df16528f182b67101c19a4227a1cc168
tc10287 <120434943+tc10287@users.noreply.github.com>
2023-08-09 20:07:24 -0700

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,7 +1,7 @@
 def prime_number(num):
     if num==1:
         return(print("1 is not considered"))
-    for i in range(2,num):
+    for i in range(2,num):                  #logic behind prime number, prime number is a number who can only be divided by the number itself
         if num%i==0:
             return(print("%d is not a prime number"%num))
     return(print("%d is a prime number"%num))
