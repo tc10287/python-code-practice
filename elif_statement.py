51f14fb6c108f4681d202a29a1a3281254301ba9
tc10287 <120434943+tc10287@users.noreply.github.com>
2023-08-09 19:52:15 -0700

    Add files via upload
    refreshing the elif statements and input statements

---
+++ elif_statement.py
@@ -0,0 +1,18 @@
+import math
+print("hello there, i am your teacher")
+marks=float(input("enter your marks \n"))
+if marks>=40:
+    print("you need to really work on yourself")
+elif (marks>50):
+    print("study hard")
+elif marks>60:
+    print("you can do it")
+elif marks>70:
+    print("good")
+elif marks>80:
+    print("keep it up")
+elif marks>90:
+        print("you need to watch more movies")
+else:
+        print("FAILED")    
+    
